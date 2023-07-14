b6bf38c109b37d309b0683e1b2609a948eedf839
Emory Al-Imam <emory@yapstudios.com>
2023-07-14 11:49:11 -0700

    Update NavigationBackport to v0.9.0

--- Package.swift
+++ Package.swift
@@ -25,17 +25,9 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
-    .package(url: "https://github.com/johnpatrickmorgan/NavigationBackport.git", exact: "0.8.1")
+    .package(url: "https://github.com/johnpatrickmorgan/NavigationBackport", exact: "0.9.0")
   ],
   targets: [
-    .target(
-      name: "_SwiftUINavigationState",
-      dependencies: [
-        .product(name: "CustomDump", package: "swift-custom-dump"),
-        .product(name: "NavigationBackport", package: "NavigationBackport"),
-        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
-      ]
-    ),
     .target(
       name: "SwiftUINavigation",
       dependencies: [
